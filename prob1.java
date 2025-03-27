4c09bbeb8e37377403047108dc7f5b665978bbaa
최인하 <choeinha@choeinhaui-MacBookPro.local>
2025-03-27 15:55:11 +0900

    입출력 문제

---
+++ prob1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class prob1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt(); // A 입력
+        int B = sc.nextInt(); // B 입력
+
+        System.out.println(A * B); // A * B 출력
+    }
+}

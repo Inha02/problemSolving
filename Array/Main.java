da08e915132e5b02fba482018649651ca80c1d4f
최인하 <bella020207@naver.com>
2025-03-31 21:40:08 +0900

    250331

---
+++ Array/Main.java
@@ -0,0 +1,11 @@
+// 정수 배열을 정렬해서 반환하는 solution( ) 함수를 완성하세요.
+package Array;
+
+import java.util.*;
+
+public class Main {
+    private static int[] solution(int[] arr) {
+        Arrays.sort(arr);
+        return arr;
+    }
+}
